c2fe51949183eb110aaa4caabb6115f272ba3f4e
Stefan Siegl <stesie@brokenpipe.de>
2020-02-07 13:39:14 +0100

    fetch + decode session data (ok, just the title)

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html
+import Http
+import Json.Decode as D
 
 
 type alias Model =
@@ -15,24 +17,46 @@ type alias Session =
 
 
 type Msg
-    = Foo
-    | Bar
+    = SetSessions (Result Http.Error (List Session))
+
+
+fetchSessions : Cmd Msg
+fetchSessions =
+    Http.get
+        { url = "https://backend.timetable.wueww.de/export/session.json"
+        , expect = Http.expectJson SetSessions sessionsFileDecoder
+        }
+
+
+sessionsFileDecoder : D.Decoder (List Session)
+sessionsFileDecoder =
+    let
+        sessionDecoder : D.Decoder Session
+        sessionDecoder =
+            D.map Session (D.field "title" D.string)
+    in
+    D.field "sessions" (D.list sessionDecoder)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { sessions = Nothing }, Cmd.none )
+    ( { sessions = Nothing }, fetchSessions )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        SetSessions (Ok sessions) ->
+            ( { model | sessions = Just sessions }, Cmd.none )
+
+        SetSessions (Err _) ->
+            ( model, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
 view _ =
     { title = "Hello World"
-    , body = [ Html.div [] [ Html.text "Hello World balrg" ] ]
+    , body = [ Html.div [] [ Html.text "Hello World" ] ]
     }
 
 
